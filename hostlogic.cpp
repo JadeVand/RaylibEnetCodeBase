44ddd21c56ad46e8cb8620c8e8ece6788003e19a
Irelia <irelia@Irelias-MacBook-Pro.local>
2022-05-04 23:13:52 -0400

    displaying hostname properlyKLJSADHKJADSDS

--- hostlogic.cpp
+++ hostlogic.cpp
@@ -12,19 +12,22 @@ void HostLogic::update(float deltatime){
     }
     auto lambda = [this](uint8_t* data,size_t length, int result){
         if(length>=sizeof(PacketHeader)+sizeof(uint64_t)){
-            struct HostPacket{
-                PacketHeader* ph;
-                uint64_t hostname;
-            };
-            HostPacket* hp = (HostPacket*)data;
-            hostname = hp->hostname;
-            printf("%p\n",hostname);
+            PacketHeader* ph = (PacketHeader*)data;
+            if(ph->packettype = kHostname&&ph->signature == NATSIGNATURE){
+                struct HostPacket{
+                    PacketHeader ph;
+                    uint64_t hostname;
+                    uint32_t extra;
+                };
+                HostPacket* hp = (HostPacket*)data;
+                hostname = hp->hostname;
+                printf("%p\n",hostname);
+            }
+            
         }
         //printf("packet size%d\n",length);
     };
     interface->quecompletion(lambda,1);
-
-    
 }
 void HostLogic::draw(int screenWidth,int screenHeight){
     
@@ -47,7 +50,7 @@ void HostLogic::draw(int screenWidth,int screenHeight){
     if(hostname){
         ClearBackground(RAYWHITE);
         std::string hostnamestring = std::to_string(hostname);
-        DrawText(hostnamestring.c_str(),screenWidth/2,screenHeight/2,10,DARKGRAY);
+        DrawText(TextFormat("Hostname: %p",hostname),screenWidth/2-100,screenHeight/2,20,RED);
     }
     
 }
